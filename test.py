e1a533b9de27f48a6a2098477c0be523815f9bc0
ceasminog <32290501+ceasminog@users.noreply.github.com>
2018-11-08 01:30:32 +0300

    added new tests

--- test.py
+++ test.py
@@ -1,22 +1,31 @@
 import unittest
 from main import find_shortest_string
+import math
 
+class  Testfind_shortest_string(unittest.TestCase):
+    def test1(self):
+        with self.assertRaises(ValueError): find_shortest_string('ab + c.', 'a', 6, 5)
 
-class test_find_shortest_string(unittest.TestCase):
-    def test_types(self):
-        with self.assertRaises(TypeError):
-            find_shortest_string('1ggg.gg', 'a', 6)
-        with self.assertRaises(TypeError):
-            find_shortest_string('ab + c.', 'a', 6, 5)
-        with self.assertRaises(TypeError):
-            find_shortest_string('ab + c.', 'a', 'y')
-        with self.assertRaises(TypeError):
-            find_shortest_string('ab + c.', 9, 8)
-        with self.assertRaises(TypeError):
-            find_shortest_string('ab + c.', 'asxdf', 2)
+    def test2(self):
+        with self.assertRaises(ValueError): find_shortest_string('ab + c.', 'a', 'y')
 
-    def test_ans(self):
-        self.assertEqual(find_shortest_string('ab + c.', 'a', 1), 2)
+    def test3(self):
+        with self.assertRaises(ValueError): find_shortest_string('ab + c.', 9, 8)
+
+    def test4(self):
+        with self.assertRaises(ValueError): find_shortest_string('ab+c.', 'asxdf', 2)
+
+    def test_ans1(self):
+        self.assertEqual(find_shortest_string(list("ab+c."), 'a', 1), 1)
+
+    def test_ans2(self):
+        self.assertEqual(find_shortest_string(list("ab.c+"), 'c', 1), 1)
+
+    def test_ans3(self):
+        self.assertEqual(find_shortest_string(list("ab.c+"), 'c', 2), math.inf)
+
+    def test_ans3(self):
+        self.assertEqual(find_shortest_string(list("a1.1.aa.+"), 'a', 4), math.inf )
 
 
 if __name__ == '__main__':
